234382c1e1037a5fa4ea7478db970ab425e9e207
Felipe Lerner <75178718+felipelerner@users.noreply.github.com>
2026-03-13 16:11:17 -0300

    Update page.tsx

--- app/que-te-apasiona/page.tsx
+++ app/que-te-apasiona/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
+import { header } from "@/components/Header";
 
 function IconInstagram() {
   return (
